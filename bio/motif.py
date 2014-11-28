808f10fcd6eaddf1f9d6622fa3c2d19c2cd47696
Michael Sanders <michael@msanders.com>
2014-11-27 19:47:21 -0800

    First shot at profile_most_probable

--- bio/motif.py
+++ bio/motif.py
@@ -1,5 +1,7 @@
 from .mutations import neighborhood
 from .hamming import hamcount
+from functools import reduce
+import operator
 
 def motif_enumeration(dna: [str], k: int, d: int) -> {str}:
     """
@@ -23,8 +25,48 @@ def motif_enumeration(dna: [str], k: int, d: int) -> {str}:
         return patterns
 
 
+def kmer_probability(profile: dict, kmer: str) -> float:
+    return reduce(operator.mul, (profile[x][i] for i, x in enumerate(kmer)))
+
+def profile_most_probable(profile: dict, text: str, k: int) -> str:
+    """
+    Input: A string Text, an integer k, and a 4 × k matrix Profile.
+    Output: A Profile-most probable k-mer in Text.
+    """
+    max_probability = 0
+    most_probable = set()
+    for start in (text[i:] for i in range(k)):
+        for i in range(len(start) - k):
+            kmer = start[i:i + k]
+            probability = kmer_probability(profile, kmer)
+            if probability > max_probability:
+                most_probable = {kmer}
+                max_probability = probability
+            elif probability == max_probability:
+                most_probable.add(kmer)
+    return most_probable
+
+
+def parse_profile(input: str) -> dict:
+    profile = {}
+    rows = ['A', 'C', 'G', 'T']
+    for i, row in enumerate(input.strip().splitlines()):
+        profile[rows[i]] = [float(x) for x in row.split()]
+    return profile
+
+
 def main():
-    print(" ".join(motif_enumeration("GCCTTGACGGTCAGGCTTACTTGAA CGTGAATGACGTCTTGTTCTGGATG CCGACCAGGTGTCTTAGAGAGATGT CTATGCCGGCGATGTGCCTTCCTGC GTCTTCAGCTCGGGTGTGGAGGGGC CAAGGTGCTGCAAAACGTTGGTCTT".split(), 5, 1))) 
+    profile = parse_profile(
+        """
+        0.258 0.197 0.318 0.242 0.273 0.273 0.258 0.318 0.273 0.212 0.409 0.258 0.212 0.212 0.258
+        0.227 0.258 0.242 0.197 0.242 0.167 0.303 0.152 0.242 0.242 0.182 0.303 0.273 0.288 0.288
+        0.242 0.227 0.227 0.273 0.227 0.212 0.152 0.273 0.197 0.273 0.197 0.242 0.258 0.273 0.242
+        0.273 0.318 0.212 0.288 0.258 0.348 0.288 0.258 0.288 0.273 0.212 0.197 0.258 0.227 0.212
+        """
+    )
+    #print(profile)
+    print(profile_most_probable(profile, "AGTGACCATTTACCCGAGGCTTCCGACAAGTAGGATTTACTCTTGGGATCCTGAGCTCGTAGTGTTTGAGGCCCTCTAGGTGCTTTTGAGGGATTGCCTGACGTGTCGGGATAAAAGCAATCAGCCACTGGACGTCTCTTGCTACCCATTTATGGCCAAATGAGTGACCAGCTATAAAGTGGTGGTGCCTTTACGAATAAAGAGTGTTCGAGCGAAAGCAAATTCGACCTGGAAGGGAGTTAGGAATTTGACGAATTGCAGACCAGTTAGAGCCACCGTGCTTCTGCTTGCCGCGGAATGGAATCACAATACAGGAGGGTCACATTTTCCCTCCAAAGACGACAGTTGCAGAGTATCCCGCGATCTACAGCGTTTGCTGTGGCCACGATCTGAATAGGGTCAGTGGTTAATTCTCCATACTATCGATGAAATCTGTGTAAATCAACAGAGGGGCGGCGACTGCCACGTCATAGAATCAGTTAAACTAGGTGCGATCCGGCCTGTAGTGCGACAATATAGCTTACTATCTTCATATGGCAACCAGCAAATTAGATTTCGGTCACAGTAACCCGATCTGTTACGCGAGGTGCACTCAGATGAGTCGCTCGGGAAACCCATAGCAACGAGGATTAGCGAATTGAAGTACCTAGGGAGGAACCCGCGAACGATAGTTTTAAGTCTGCGCCGAATCGTTTTACTGAACGTGTATCGGTGCCAATATGCTGAATGAACACACTACACGTTTATAGCCCAAAACGTTTAAAGCGAGGTGTATCTTGGAACCGGCAAGCATACGCTGCACATCTAGCTTCGCATATAACGGCAGTTGAGGAATCTAATGGGACACTTGCGGTGTGCTTTGTCGGAGAAGTTAGAAACAGCAGTTGGTTCGGCGGGGTCCTGACTTAGGGCACGGCGAACATACAGCGCTAGGAGTCAGCAAGCATATTGGAAACTTAACGCCTTCTTGAACCCTTAATCTAAATGGCAAAAATATGCG", 15))
+    #print(" ".join(motif_enumeration("GCCTTGACGGTCAGGCTTACTTGAA CGTGAATGACGTCTTGTTCTGGATG CCGACCAGGTGTCTTAGAGAGATGT CTATGCCGGCGATGTGCCTTCCTGC GTCTTCAGCTCGGGTGTGGAGGGGC CAAGGTGCTGCAAAACGTTGGTCTT".split(), 5, 1)))
 
 if __name__ == '__main__':
     main()
--- bio/tests/motif.py
+++ bio/tests/motif.py
@@ -1,4 +1,4 @@
-from ..motif import motif_enumeration
+from ..motif import motif_enumeration, parse_profile, profile_most_probable
 from ._lib import find_datasets
 
 
@@ -15,8 +15,31 @@ def test_motif_enumeration():
         assert output == output_sample, "{0} != {1}".format(output, output_sample)
 
 
+def test_profile_most_probable():
+    def parse_input(text):
+        lines = text.splitlines()
+        return "\n".join(lines[:-2]), lines[-2], lines[-1]
+
+    dataset = [
+        (("""
+          0.2 0.2 0.3 0.2 0.3
+          0.4 0.3 0.1 0.5 0.1
+          0.3 0.3 0.5 0.2 0.4
+          0.1 0.2 0.1 0.1 0.2
+          """, "ACCTGTTTATTGCCTAAGTTCCGAACAAACCCAATATAGCCCGAGGGCCT", 5),
+          "CCGAG")
+    ] + find_datasets("profile_most_probable", parse_input)
+
+    for input_sample, output_sample in dataset:
+        profile, text, k = input_sample
+        profile, k = parse_profile(profile), int(k)
+        output = profile_most_probable(profile, text, k)
+        assert output_sample in output, "{0} != {1}".format(output, output_sample)
+
+
 def main():
     test_motif_enumeration()
+    test_profile_most_probable()
     print("Success!")
 
 if __name__ == '__main__':
