22382656f7426eed7d35b35b0b32363b0a294e89
Michael Sanders <michael@msanders.com>
2014-12-01 12:41:57 -0800

    Add textbook implementation of median_string

--- bio/median.py
+++ bio/median.py
@@ -1,6 +1,6 @@
 import math
 from .hamming import hamming_distance
-from .numberpattern import all_nucleotides
+from .numberpattern import all_nucleotides, number_to_pattern
 
 def min_distance(pattern, text):
     """
@@ -26,17 +26,36 @@ def min_dna_distance(pattern, dna):
     return sum(min_distance(pattern, text) for text in dna)
 
 
+def distance_between_pattern_and_dna(pattern: str, dna: [str]) -> int:
+    """
+    Input: A string Pattern followed by a collection of strings Dna.
+    Output: d(Pattern, Dna).
+    """
+    k = len(pattern)
+    distance = 0
+    for text in dna:
+        min_distance = float("inf")
+        for i in range(len(text) - k + 1):
+            kmer = text[i:i + k]
+            kmer_distance = hamming_distance(pattern, kmer)
+            if min_distance > kmer_distance:
+                min_distance = kmer_distance
+        distance += min_distance
+    return distance
+
+
 def median_string(dna, k):
     distance = float("inf")
     medians = []
-    for pattern in all_nucleotides(k, stringify=True):
-        d = min_dna_distance(pattern, dna)
-        if d < distance:
-            distance = d
-            median = [pattern]
-        elif d == distance:
-            median.append(pattern)
-    return median
+    for i in range(4 ** k):
+        pattern = number_to_pattern(i, k)
+        pattern_distance = distance_between_pattern_and_dna(pattern, dna)
+        if distance > pattern_distance:
+            distance = pattern_distance
+            medians = [pattern]
+        elif distance == pattern_distance:
+            medians.append(pattern)
+    return medians
 
 
 def main():
--- bio/tests/median.py
+++ bio/tests/median.py
@@ -1,4 +1,4 @@
-from ..median import median_string
+from ..median import median_string, distance_between_pattern_and_dna
 from ._lib import find_datasets
 
 def test_median_string():
@@ -14,8 +14,23 @@ def test_median_string():
         assert output_sample in output, "{0} != {1}".format(output, output_sample)
 
 
+def test_distance_between_pattern_and_dna():
+    dataset = [
+        (("AAA", "TTACCTTAAC GATATCTGTC ACGGCGTTCG CCCTAAAGAG CGTCAGAGGT"), 5),
+    ] + find_datasets("distance_between_pattern_and_dna",
+                      lambda x: x.splitlines())
+
+    for input_sample, output_sample in dataset:
+        pattern, dna = input_sample
+        dna = dna.split()
+        output = distance_between_pattern_and_dna(pattern, dna)
+        output_sample = int(output_sample)
+        assert output_sample == output, "{0} != {1}".format(output, output_sample)
+
+
 def main():
     test_median_string()
+    test_distance_between_pattern_and_dna()
     print("Success!")
 
 
