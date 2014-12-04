1b29d096082429718a98aeb6db13f41a764c8dfe
Michael Sanders <michael@msanders.com>
2014-12-04 15:32:26 -0800

    Add genome_path_string definition & tests

--- bio/composition.py
+++ bio/composition.py
@@ -10,8 +10,28 @@ def string_composition(text: str, k: int) -> [str]:
     return kmers
 
 
+def genome_path_string(kmers: [str]):
+    """
+    String Spelled by a Genome Path Problem: Reconstruct a string from its
+                                             genome path.
+    Input: A sequence of k-mers Pattern1, … ,Pattern[n] such that the last k - 1
+           symbols of Pattern[i] are equal to the first k-1 symbols of
+           Pattern[i+1] for 1 ≤ i ≤ n-1.
+    Output: A string Text of length k+n-1 such that the i-th k-mer in Text is
+            equal to Pattern[i] (for 1 ≤ i ≤ n).
+    """
+    k = len(kmers[0])
+    pattern = kmers[0]
+    for kmer in kmers[1:]:
+        pattern += kmer[-1]
+    return pattern
+
+
 def main():
-    print("\n".join(string_composition("CAATCCAAC", 5)))
+    with open("bio/data/dataset_198_3.txt") as f:
+        kmers = f.read().strip().split()
+        print(genome_path_string(kmers))
+    #print("\n".join(string_composition("CAATCCAAC", 5)))
 
 
 if __name__ == '__main__':
--- bio/tests/composition.py
+++ bio/tests/composition.py
@@ -1,4 +1,4 @@
-from ..composition import string_composition
+from ..composition import string_composition, genome_path_string
 from ._lib import find_datasets
 import numpy as np
 
@@ -16,8 +16,21 @@ def test_string_composition():
         assert np.array_equal(output, output_sample), "{0} != {1}".format(output, output_sample)
 
 
+def test_genome_path_string():
+    dataset = [
+        ("ACCGA CCGAA CGAAG GAAGC AAGCT", "ACCGAAGCT")
+    ] + find_datasets("genome_path_string")
+
+    for input_sample, output_sample in dataset:
+        kmers = input_sample.split()
+        output = genome_path_string(kmers)
+        output_sample = output_sample
+        assert output == output_sample, "{0} != {1}".format(output, output_sample)
+
+
 def main():
     test_string_composition()
+    test_genome_path_string()
     print("Success!")
 
 if __name__ == '__main__':
