2d6b228e1de106c554117e40df7243b4bd86f0fd
Michael Sanders <michael@msanders.com>
2015-01-02 16:43:36 -0500

    Add longest_dag_path and topological ordering

--- bio/manhatten.py
+++ bio/manhatten.py
@@ -1,4 +1,26 @@
 import numpy as np
+import random
+import re
+from .graph import build_di_graph
+from copy import deepcopy
+
+
+def parse_graph(text: str, weighted: bool = False) -> dict:
+    output = text.strip().replace(" -> ", "->").split()
+    graph = {}
+    for x in output:
+        key, values = x.split("->")
+        if weighted:
+            key = int(key)
+        if key not in graph:
+            graph[key] = []
+        if weighted:
+            graph[key] = sorted(graph[key] + [tuple(
+                int(x) for x in values.split(":"))]
+            )
+        else:
+            graph[key] = sorted(graph[key] + values.split(","))
+    return graph
 
 
 def manhatten_tourist(n: int, m: int, down: np.ndarray, right: np.ndarray):
@@ -71,7 +93,111 @@ def output_lcs(backtrack: np.ndarray, v: str, w: str):
     return "".join(reversed(lcs))
 
 
+def outgoing_edges(graph: dict) -> set:
+    return set(x for values in graph.values() for x in values)
+
+
+def topological_ordering(graph: dict) -> list:
+    graph = deepcopy(graph)
+    order = []
+    values = outgoing_edges(graph)
+    candidates = set(graph.keys()) - values
+
+    while candidates:
+        node = random.sample(candidates, 1)[0]
+        order.append(node)
+        candidates.remove(node)
+
+        outgoing_nodes = graph.get(node, []).copy()
+        graph[node] = []
+        values = outgoing_edges(graph)
+
+        for node in outgoing_nodes:
+            if node not in values:
+                candidates.add(node)
+
+    if outgoing_edges(graph):
+        raise ValueError("Input graph is not a DAG")
+    return order
+
+
+def unweighted_graph(graph: dict) -> dict:
+    unweighted = {}
+    for key in graph:
+        unweighted[key] = [x[0] for x in graph[key]]
+    return unweighted
+
+
+def incoming_edges(graph: dict, node: object) -> list:
+    return [key for key in graph if node in graph[key]]
+
+
+def edge_with_weight(graph: dict, in_node: object, out_node: object) -> int:
+    return [x for x in graph[in_node] if x[0] == out_node][0]
+
+
+def longest_dag_path(graph: dict, source: int, sink: int) -> (int, list):
+    unweighted = unweighted_graph(graph)
+    s = {node: float("-inf")
+         for node in set(graph) | outgoing_edges(unweighted)}
+    s[source] = 0
+
+    top_order = topological_ordering(unweighted)
+    top_order = top_order[top_order.index(source) + 1:top_order.index(sink) + 1]
+
+    backtrack = {node: None for node in top_order}
+    for node in top_order:
+        for key in incoming_edges(unweighted, node):
+            node, weight = edge_with_weight(graph, key, node)
+            weight += s[key]
+            if weight > s[node]:
+                s[node], backtrack[node] = weight, key
+
+    # Backtrack to get the longest path.
+    path = [sink]
+    while path[0] != source:
+        path.insert(0, backtrack[path[0]])
+
+    return s[sink], path
+
+
+
 def main():
+    #text = """
+    #0 -> 1,11,12,14,15,16,17,18,2,3,6,7,8
+    #1 -> 11,14,17,18,19,2,7,8
+    #10 -> 12,13,14,15,18,19
+    #11 -> 13,14,17
+    #12 -> 15,16,19
+    #13 -> 18
+    #14 -> 15,16,17,19
+    #15 -> 17,19
+    #16 -> 17
+    #17 -> 19
+    #2 -> 14,15,19,3,9
+    #3 -> 14,15,17,4,5,7,8,9
+    #4 -> 11,13,14,15,18,6,8
+    #5 -> 10,12,6,8,9
+    #6 -> 10,12,13,15,17,18,19,7
+    #7 -> 10,12,13,14,16,17,19,9
+    #8 -> 10,13,14,15,16,18,9
+    #9 -> 12,14,17
+    #"""
+
+    #graph = parse_graph(text)
+    #print(", ".join(topological_ordering(graph)))
+    #return
+    text = """
+    0->1:7
+    0->2:4
+    2->3:2
+    1->4:1
+    3->4:3
+    """
+    graph = parse_graph(text, weighted=True)
+    print(graph)
+    print(longest_dag_path(graph, 0, 4))
+    return
     text = """AACCTTGG
     ACACTGTGA"""
     v, w = text.split()
--- bio/tests/manhatten.py
+++ bio/tests/manhatten.py
@@ -1,5 +1,6 @@
 from ..manhatten import (
-    manhatten_tourist, parse_tourist_input, output_lcs, lcs_backtrack, lcs_path
+    manhatten_tourist, parse_tourist_input, output_lcs, lcs_backtrack,
+    lcs_path, longest_dag_path, parse_graph
 )
 from ._lib import find_datasets
 
@@ -28,7 +29,6 @@ def test_manhatten_tourist():
 def test_output_lcs():
     dataset = [("AACCTTGG ACACTGTGA", "AACTTG")] + find_datasets("output_lcs")
 
-
     for input_sample, output_sample in dataset:
         v, w = input_sample.strip().split()
         backtrack = lcs_backtrack(lcs_path(v, w))
@@ -36,9 +36,39 @@ def test_output_lcs():
         assert output == output_sample, "{0} != {1}".format(output, output_sample)
 
 
+def test_longest_dag_path():
+    def parse_input(text: str) -> (int, int, str):
+        lines = text.splitlines()
+        return int(lines[0]), int(lines[1]), "\n".join(lines[2:])
+
+    def parse_output(text: str) -> (int, list):
+        lines = text.splitlines()
+        return int(lines[0]), [int(x) for x in lines[1].split("->")]
+
+    dataset = [((
+        0, 4,
+        """
+        0->1:7
+        0->2:4
+        2->3:2
+        1->4:1
+        3->4:3
+        """
+    ), (9, [0, 2, 3, 4]))] + find_datasets(
+        "longest_dag_path", parse_input, parse_output
+    )
+
+    for input_sample, output_sample in dataset:
+        source, sink, graph = input_sample
+        graph = parse_graph(graph, weighted=True)
+        output = longest_dag_path(graph, source, sink)
+        assert output == output_sample, "{0} != {1}".format(output, output_sample)
+
+
 def main():
-    test_manhatten_tourist()
-    test_output_lcs()
+    #test_manhatten_tourist()
+    #test_output_lcs()
+    test_longest_dag_path()
     print("Success!")
 
 
